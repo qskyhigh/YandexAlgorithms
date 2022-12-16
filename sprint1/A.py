ae8c95e8c1ed464fc395726b47bec2d881286f7d
qskyhigh <q.sky.high@gmail.com>
2022-12-16 17:01:14 +0300

    Added solution

---
+++ sprint1/A.py
@@ -0,0 +1,26 @@
+from typing import List
+import unittest
+from dataclasses import dataclass
+class Solution:
+
+    def func(self, numbers: List[int]) -> int:
+        return numbers[0] * numbers[1] * numbers[1] + numbers[2] * numbers[1] + numbers[3]
+
+
+@dataclass
+class TestCase:
+    numbers: List[int]
+    expectation: int
+
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        solution = Solution()
+
+        tests = [
+            TestCase([-8, -5, -2, 7], -183),
+            TestCase([8, 2, 9, -10], 40)
+        ]
+
+        for t in tests:
+            with self.subTest(t):
+                self.assertEqual(solution.func(t.numbers), t.expectation)
